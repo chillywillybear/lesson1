6f3f42fb02170e725313f67aadbcc26fdec8b75a
Damir <example@gmail.com>
2018-12-09 01:14:43 +0300

    Это наш первый коммит

---
+++ lesson 1/script.js
@@ -0,0 +1,19 @@
+var money = +prompt("Ваш бюджет на месяц?", "");
+var time = +prompt("Введите дату в формате YYYY-MM-DD", "");
+var appData = {
+    budget : money,
+    timeData : time,
+    expenses : {   
+},
+    optionalExpenses : {  
+},
+    income: [],
+    savings : false
+}
+var option1 = +prompt("Введите обязательную статью расходов в этом месяце", "");
+var option2 = +prompt("Во сколько обойдется?", "");
+var option3 = +prompt("Введите обязательную статью расходов в этом месяце", "");
+var option4 = +prompt("Во сколько обойдется?", "");
+appData.expenses.option1 = option2;
+appData.expenses.option3 = option4;
+alert(appData.budget / 30);
